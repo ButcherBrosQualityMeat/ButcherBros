7d2ce7702a685eb6c50b9b24a031f6dc6ece891b
dalton <daltonspinas@gmail.com>
2019-01-08 15:25:45 -0800

    added routes for category by id, with eager loaded products

---
+++ server/api/categories.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const {Category, Product} = require('../db/models')
+module.exports = router
+
+//returns all categories
+router.get('/', async (req, res, next) => {
+  try {
+    const allCategories = await Category.findAll()
+    res.json(allCategories)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// returns a specific category with related products eager loaded
+router.get('/:id', async (req, res, next) => {
+  const categoryId = req.params.id
+  try {
+    const singleCategory = await Category.findByPk(categoryId, {
+      include: [{model: Product}]
+    })
+    res.json(singleCategory)
+  } catch (err) {
+    next(err)
+  }
+})
