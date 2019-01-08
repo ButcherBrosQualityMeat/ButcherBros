5e02521a0974f4e2d0a8121acb0e28b170d57e5b
James <james.chris.santos@gmail.com>
2019-01-08 14:48:32 -0800

    (feat) added homepage user view

---
+++ client/components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+const HomePage = props => {
+  //const { products } = props;
+  return (
+    <div>
+      <h1>Butcher Bros</h1>
+      <hr />
+
+      <div>
+        <h3>Categories</h3>
+        <ul>
+          <li>Beef</li>
+          <li>Pork</li>
+          <li>Chicken</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default connect(mapState, mapDispatch)(HomePage)
