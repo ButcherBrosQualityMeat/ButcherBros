8c139fdcc5dc860222e0e56d542a1da3f75f148e
Frank Compston <compstonfrank@gmail.com>
2019-01-09 21:20:45 -0800

    Feature: revamped landing page

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,81 +5,71 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div className="div-nav">
-    <nav>
-      {isLoggedIn ? (
-        <nav
-          id="navbar"
-          className="navbar navbar-expand-lg navbar-light bg-light"
+  <nav
+    id="navbar"
+    className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+  >
+    {isLoggedIn ? (
+      <div className="container">
+        <a className="navbar-brand" href="/homepage">
+          Butcher Bros
+          {'    '}
+          <img
+            height="40px"
+            width="40px"
+            src="https://images.vexels.com/media/users/3/143248/isolated/preview/9a073ffe6b6bd3508dd0f6e4da820c9a-steak-stroke-icon-by-vexels.png"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <a className="navbar-brand" href="#">
-            ButcherBros
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/products">
-                  Products
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Categories
-                </a>
-                <div
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <a className="dropdown-item" href="categories/1">
-                    Beef
-                  </a>
-                  <a className="dropdown-item" href="categories/2">
-                    Pork
-                  </a>
-                  <a className="dropdown-item" href="categories/3">
-                    Chicken
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <a className="nav-link" href="/">
-              Log Out <span className="sr-only">(current)</span>
-            </a>
-          </div>
-        </nav>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/homepage">
+                Home
+                <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/products">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/cart">
+                Cart
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/cart">
+                Logout
+              </a>
+            </li>
+          </ul>
         </div>
-      )}
-    </nav>
-  </div>
+      </div>
+    ) : (
+      <div>
+        {/* The navbar will show these links before you log in */}
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    )}
+  </nav>
 )
 
 /**
