64e25df8321df4b9927049efc375494274b3607a
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-10 10:09:46 -0800

    Initialize dummy data for cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -30,15 +30,16 @@ export const addItemToCart = item => {
   }
 }
 
-// Reducer
-// Cart is an array of objects representing items.
+// Cart Reducer
+// Cart.contents: an array of objects representing items.
 // Each object has two properties: quantity and productId
+// Cart.orderId: if user is logged in, orderId of order stored in database
 export default function(cart = dummyCart, action) {
   switch (action.type) {
     case ADD_TO_CART:
       return {
         ...cart,
-        contenst: cart.contents.concat(action.item)
+        contents: cart.contents.concat(action.item)
       }
     default:
       return cart
