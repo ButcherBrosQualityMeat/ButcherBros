f9aedbd95b7e99e536a123dbf5bba6bdb88e4b06
dalton <daltonspinas@gmail.com>
2019-01-10 12:06:21 -0800

    feat: add to cart button on AllProducts component

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {addItemToCart} from '../store/cart'
 
 class AllProducts extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(evt) {
+    evt.preventDefault()
+    const productId = evt.target.id
+    console.log(productId)
+    this.props.addItemToCart({productId, quantity: 1})
+  }
   render() {
     if (this.props.info.length === 0) {
       return <div>Please standy, loading products...</div>
@@ -10,17 +22,22 @@ class AllProducts extends React.Component {
     return (
       <div>
         <h1>List of Products:</h1>
-        <div>
+        <div className="card-group">
           {this.props.info.allProducts.map(product => (
-            <div key={product.id}>
+            <div key={product.id} className="card">
               <Link to={`/products/${product.id}`}>
-                <h1>{product.name}</h1>
-                <img src={product.imageUrl} width="300px" heigth="300px" />
+                <img src={product.imageUrl} className="card-img-top" />
               </Link>
-              <div>
-                <p>{product.description}</p>
-                <button>Add to Cart</button>
-              </div>
+              <h5 className="card-title">{product.name}</h5>
+              <p className="card-text">{product.description}</p>
+              <a
+                href="#"
+                className="btn btn-primary"
+                id={product.id}
+                onClick={this.handleClick}
+              >
+                Add to Cart
+              </a>
             </div>
           ))}
         </div>
@@ -29,8 +46,12 @@ class AllProducts extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapState = state => ({
   info: state.product
 })
 
-export default connect(mapStateToProps)(AllProducts)
+const mapDispatch = dispatch => ({
+  addItemToCart: productObj => dispatch(addItemToCart(productObj))
+})
+
+export default connect(mapState, mapDispatch)(AllProducts)
