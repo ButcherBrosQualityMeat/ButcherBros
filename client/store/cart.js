58208bf09c5347f4155b0bfe9b59d10109100cd4
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-10 21:41:17 -0800

    Feature: Create Thunks to store cart on req.session

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,20 +1,14 @@
+import axios from 'axios'
+
 // Initial State
 const emptyCart = {
   contents: [],
   orderId: null
 }
 
-const dummyCart = {
-  contents: [
-    {quantity: 1, productId: 1},
-    {quantity: 2, productId: 2},
-    {quantity: 3, productId: 3}
-  ],
-  orderId: null
-}
-
 // ACTION TYPES
 const ADD_TO_CART = 'ADD_TO_CART'
+const RETRIEVED_CART = 'RETRIEVED_CART'
 
 // Action Creators
 // item is an object with two properties:
@@ -25,6 +19,11 @@ const addToCart = item => ({
   item
 })
 
+const retrievedCart = cart => ({
+  type: RETRIEVED_CART,
+  cart
+})
+
 // Thunk Creators
 export const addItemToCart = item => {
   return dispatch => {
@@ -33,11 +32,32 @@ export const addItemToCart = item => {
   }
 }
 
+// initial fetch of cart from server (if it exists)
+export const fetchCart = () => {
+  return async dispatch => {
+    const response = await axios.get('api/cart')
+    const cart = response.data
+    const action = retrievedCart(cart)
+    dispatch(action)
+  }
+}
+
+// update cart on server
+export const updateCart = cart => {
+  return async dispatch => {
+    try {
+      await axios.put('api/cart', cart)
+    } catch (error) {
+      console.error('Cart not updated: ', error)
+    }
+  }
+}
+
 // Cart Reducer
 // Cart.contents: an array of objects representing items.
 // Each object has two properties: quantity and productId
 // Cart.orderId: if user is logged in, orderId of order stored in database
-export default function(cart = dummyCart, action) {
+export default function(cart = emptyCart, action) {
   switch (action.type) {
     case ADD_TO_CART:
       // check if product already exists in cart
@@ -69,6 +89,8 @@ export default function(cart = dummyCart, action) {
           contents: cart.contents.concat(action.item)
         }
       }
+    case RETRIEVED_CART:
+      return action.cart
     default:
       return cart
   }
