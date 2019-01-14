a9792f768ef2ecc90b9c8ea8d46ec668b148a061
James <james.chris.santos@gmail.com>
2019-01-14 13:34:24 -0800

    fix: naming bug fixed

--- client/components/index.js
+++ client/components/index.js
@@ -3,12 +3,12 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './Nav-bar'
+export {default as Navbar} from './Navbar'
 export {default as UserHome} from './UserHome'
 export {default as HomePage} from './HomePage'
 export {default as SinglePageView} from './SinglePageView'
 export {default as About} from './About'
-export {default as Footer} from './Foot-er'
+export {default as Footer} from './Footer'
 export {default as Cart} from './Cart'
 export {default as AllProducts} from './AllProducts'
 export {default as SingleCategory} from './SingleCategory'
