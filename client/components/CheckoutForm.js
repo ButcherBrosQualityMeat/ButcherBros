4506a38328c4636455fc39ae81eb388a307ce619
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-14 09:00:33 -0800

    Refactor: Implement local state for CheckoutForm.js

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -3,6 +3,26 @@ import {connect} from 'react-redux'
 import {Cart} from './'
 
 class CheckoutForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      email: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const name = event.target.name
+    const value = event.target.value
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   render() {
     return (
       <div>
@@ -10,13 +30,37 @@ class CheckoutForm extends React.Component {
         <Cart />
         <form>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" size="50" required />
+          <input
+            type="text"
+            name="firstName"
+            size="50"
+            onChange={this.handleChange}
+            required
+          />
           <label htmlFor="lastName">Last Name</label>
-          <input type="text" name="lastName" size="50" required />
+          <input
+            type="text"
+            name="lastName"
+            size="50"
+            onChange={this.handleChange}
+            required
+          />
           <label htmlFor="address">Address</label>
-          <input type="text" name="address" size="50" required />
+          <input
+            type="text"
+            name="address"
+            size="50"
+            onChange={this.handleChange}
+            required
+          />
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" size="50" required />
+          <input
+            type="text"
+            name="email"
+            size="50"
+            onChange={this.handleChange}
+            required
+          />
           <button type="submit">Submit Order</button>
         </form>
       </div>
