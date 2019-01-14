cd2ceceae9fbc0a89e1470f33fa62803f1cf99d4
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-14 11:28:31 -0800

    Feat: Order View displays Order Info

--- client/components/OrderView.js
+++ client/components/OrderView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import {Link, withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 
 class OrderView extends React.Component {
@@ -13,15 +13,51 @@ class OrderView extends React.Component {
 
   async componentDidMount() {
     // fetch order from server
+    const response = await axios.get(
+      `/api/orders/${this.props.match.params.orderId}`
+    )
+    const order = response.data
+    console.log(order)
+    this.setState(prevState => ({
+      ...prevState,
+      order
+    }))
   }
 
   render() {
-    if (!this.state.order.id) {
+    if (!this.state.order.id || this.props.products.length === 0) {
       return <div>...loading</div>
     }
-
-    return <div>Order Info Here</div>
+    const {firstName, lastName, address, contents, email, id} = this.state.order
+    const products = this.props.products
+    return (
+      <div>
+        <h3>Order Info</h3>
+        <div>Order Number: {id}</div>
+        <div>Name: {`${firstName} ${lastName}`}</div>
+        <div>Address: {address}</div>
+        <div>Email: {email}</div>
+        <div>
+          {contents.map(item => {
+            const product = products.find(e => e.id === item.productId)
+            return (
+              <div key={product.id}>{`Name: ${product.name}, Quantity: ${
+                item.quantity
+              }`}</div>
+            )
+          })}
+        </div>
+      </div>
+    )
   }
 }
 
-export default withRouter(OrderView)
+const mapState = state => {
+  return {
+    products: state.product.allProducts
+  }
+}
+
+const OrderContainer = connect(mapState, null)(OrderView)
+
+export default withRouter(OrderContainer)
