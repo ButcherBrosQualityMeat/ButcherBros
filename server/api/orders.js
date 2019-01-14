00e10bbc5b47e65f28a216b070a5d43b552051aa
Henry Do <661034+vitaminh@users.noreply.github.com>
2019-01-14 10:56:07 -0800

    Feat: Create route to retrieve order by orderId

--- server/api/orders.js
+++ server/api/orders.js
@@ -3,6 +3,16 @@ const {Order} = require('../db/models')
 
 module.exports = router
 
+// GET
+router.get('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.orderId)
+    res.json(order)
+  } catch (error) {
+    next(error)
+  }
+})
+
 // POST
 router.post('/', async (req, res, next) => {
   try {
